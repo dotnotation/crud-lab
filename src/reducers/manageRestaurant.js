ac47573bd42a23ddf506b7180e4bc3fb26deb850
dotnotation <dotinmotionproductions@gmail.com>
2022-02-24 15:40:04 -0500

    commit tree 915b387044ffcf76a9d37b8ecb5ad241cef163dd

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,6 +1,34 @@
+
 import cuid from 'cuid';
 export const cuidFn = cuid;
  
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants:[],
+  reviews: []
+}, action) {
+    switch(action.type) {
+        case "ADD_RESTAURANT":
+            let restaurant = {
+                id: cuid(),
+                text: action.text
+                }
+            return {...state, restaurants: [...state.restaurants, restaurant]}
 
+        case "DELETE_RESTAURANT":
+            return {...state, restaurants: state.restaurants.filter(rest => rest.id !== action.restaurantId)}
+            
+        case "ADD_REVIEW":
+            let review = {
+                restaurantId: action.restaurantId,
+                text: action.text,
+                id: cuid()
+            }
+            return {...state, reviews: [...state.reviews, review]}
+        
+        case "DELETE_REVIEW":
+            return {...state, reviews: [...state.reviews.filter(review => review.id !== action.reviewId)]}
+        
+        default:
+            return state
+  }
 }
